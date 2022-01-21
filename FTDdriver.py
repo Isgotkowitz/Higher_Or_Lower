42e1267b63f4440c0d02c9db9681af1c1fbfa699
Ed, XPS Personal <isgotkowitz@gmail.com>
2022-01-21 01:20:29 -0600

    current version of FTDdriver.py

--- FTDdriver.py
+++ FTDdriver.py
@@ -1,64 +1,64 @@
 """
 Driver for a modified version of the drinking card game Fuck the Dealer
 """
+# import os
 import sys
+# sys.path.append(".")
 import random as rand
 import math
 import numpy as np
-from Fuck_the_Dealer import FTDgame.py
-from FTDgame.py import FTDgame
 
-# class FTDgame():
-#     """ Object representation of deck of cards being played with """
-#     def __init__(self, valueRange, numSuits):
-#         self.valueRange = valueRange
-#         self.numSuits = numSuits
-#         self.cardsInDeck = (numSuits * valueRange)
-#         self.reValues = np.arange(valueRange)
-#         self.deck = np.arange(valueRange)
-#         self.deck.fill(numSuits)
-#         self.board = np.zeros(valueRange)
-#         # self.topCard = self.drawCard()
-#
-#     def drawCard(self):
-#         # Draws a random card and removes it from the deck
-#         randInt = rand.randint(0, len(self.reValues) - 1)
-#         card = self.reValues[randInt]
-#         if self.reValues.size == 1:
-#             self.deck[self.reValues[0]]
-#             self.deck[card] = 0
-#         elif self.deck[card] > 1:
-#             self.deck[card] -= 1
-#         else:
-#             self.deck[card] = 0
-#             self.reValues = np.delete(self.reValues, randInt)
-#         self.cardsInDeck -= 1
-#         return card
-#
-#     def firstGuess(self, guess):
-#         self.topCard = self.drawCard()
-#         # Takes in first guess from the player
-#         if guess == self.topCard:
-#             self.board[self.topCard] += 1
-#             # self.topCard = self.drawCard()
-#             return -5
-#         elif guess > self.topCard:
-#             return 0
-#         else:
-#             return 1
-#
-#     def secondGuess(self, guess):
-#         # Takes in second guess from the player
-#         if guess == self.topCard:
-#             self.board[self.topCard] += 1
-#             # self.topCard = self.drawCard()
-#             return guess, -3
-#         else:
-#             points = abs(self.topCard - guess)
-#             TC = self.topCard
-#             self.board[self.topCard] += 1
-#             # self.topCard = self.drawCard()
-#             return TC, points
+class FTDgame():
+    """ Object representation of deck of cards being played with """
+    def __init__(self, valueRange, numSuits):
+        self.valueRange = valueRange
+        self.numSuits = numSuits
+        self.cardsInDeck = (numSuits * valueRange)
+        self.reValues = np.arange(valueRange)
+        self.deck = np.arange(valueRange)
+        self.deck.fill(numSuits)
+        self.board = np.zeros(valueRange)
+        # self.topCard = self.drawCard()
+
+    def drawCard(self):
+        # Draws a random card and removes it from the deck
+        randInt = rand.randint(0, len(self.reValues) - 1)
+        card = self.reValues[randInt]
+        if self.reValues.size == 1:
+            self.deck[self.reValues[0]]
+            self.deck[card] = 0
+        elif self.deck[card] > 1:
+            self.deck[card] -= 1
+        else:
+            self.deck[card] = 0
+            self.reValues = np.delete(self.reValues, randInt)
+        self.cardsInDeck -= 1
+        return card
+
+    def firstGuess(self, guess):
+        self.topCard = self.drawCard()
+        # Takes in first guess from the player
+        if guess == self.topCard:
+            self.board[self.topCard] += 1
+            # self.topCard = self.drawCard()
+            return -5
+        elif guess > self.topCard:
+            return 0
+        else:
+            return 1
+
+    def secondGuess(self, guess):
+        # Takes in second guess from the player
+        if guess == self.topCard:
+            self.board[self.topCard] += 1
+            # self.topCard = self.drawCard()
+            return guess, -3
+        else:
+            points = abs(self.topCard - guess)
+            TC = self.topCard
+            self.board[self.topCard] += 1
+            # self.topCard = self.drawCard()
+            return TC, points
 
 def interactiveGamePlay():
         print("Welcome to the interactive game play mode of Fuck the Dealer")
@@ -143,22 +143,31 @@ def binarySearch():
     print("Value range: 1 to {}, Number of suits: {}".format(valueRange, numSuits))
 
     points = 0
-    while game.cardsInDeck >= 0:
+    while game.cardsInDeck > 0:
+        print("reValues:", game.reValues)
+        print("board:", game.board)
         reValues = game.reValues
         # First guess portion
-        firstGuess = reValues[math.floor(reValues.size / 2)]
+        firstGuess = reValues[math.floor(reValues.size * (1/2))] + 1
+        print("first guess:", firstGuess)
         # firstGuess = game.reValues[math.floor(game.reValues.size / 2)]
         firstAnswer = game.firstGuess(firstGuess)
         if firstAnswer == -5:
             points -= 5
-            continue
+            # continue
         # Second guess portion
-        if firstAnswer == 0:
-            secondGuess = reValues[math.floor(reValues.size / 4)]
         else:
-            secondGuess = reValues[math.floor(reValues.size * (3/4))]
-        value, secondAnswer = game.secondGuess(secondGuess)
-        points += secondAnswer
+            if firstAnswer == 0:
+                print("Lower")
+                secondGuess = reValues[math.floor(reValues.size * (1/4))] + 1
+            else:
+                print("Higher")
+                secondGuess = reValues[math.floor(reValues.size * (3/4))] + 1
+            value, secondAnswer = game.secondGuess(secondGuess)
+            print("second guess:", secondGuess)
+            points += secondAnswer
+            print("value:", value)
+        print("\n")
     print("Total points:", points)
 
 def main():
