1081cc6ab9d5a94a0eb0c27ccaeb2389706b3362
Ed, XPS Personal <isgotkowitz@gmail.com>
2022-01-23 17:22:36 -0600

    Added a file for testing

---
+++ HOL_testing.py
@@ -0,0 +1,11 @@
+"""
+Provides the main file from which testing and gameplay are run
+"""
+from HOL_game_modes import *
+
+def main():
+    interactiveGamePlay()
+    # binarySearch()
+
+if __name__ == "__main__":
+    main()
