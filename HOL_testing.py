32d4f1d23031f2fd2008162088a46d7956390e39
Ed, XPS Personal <isgotkowitz@gmail.com>
2022-01-23 20:05:41 -0600

    testing for binary search alg

--- HOL_testing.py
+++ HOL_testing.py
@@ -4,8 +4,13 @@ Provides the main file from which testing and gameplay are run
 from HOL_game_modes import *
 
 def main():
-    interactiveGamePlay()
+    # interactiveGamePlay()
     # binarySearch()
+    numGames = 100
+    totalPoints = 0
+    for i in range(numGames):
+        totalPoints += binarySearchNP(4, 13)
+    print("average points:", totalPoints / numGames)
 
 if __name__ == "__main__":
     main()
