de6ea92cddf82fd396c7df59d6641119b052229a
Ed, XPS Personal <isgotkowitz@gmail.com>
2022-01-29 21:19:18 -0600

    Changed testing.py's parameters

--- testing.py
+++ testing.py
@@ -9,7 +9,7 @@ def main():
     numGames = 20000
     totalPoints = 0
     for i in range(numGames):
-        totalPoints += binarySearchNP(4, 13)
+        totalPoints += binarySearchNP(1, 20)
     print("average points over {} games: {}".format(numGames, totalPoints / numGames))
 
 if __name__ == "__main__":
